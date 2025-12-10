85779bc6708ddead1ecec8f33e8ea334956d960d
medby666 <medconde666@gmail.com>
2025-12-10 19:36:17 +0000

    Restauration des migrations après reset

---
+++ Collectivite/Migrations/20251208114715_initCreate_2.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Collectivite.Migrations
+{
+    /// <inheritdoc />
+    public partial class initCreate_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MouvementId",
+                table: "EcritureComptables",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EcritureComptables_MouvementId",
+                table: "EcritureComptables",
+                column: "MouvementId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EcritureComptables_Mouvement_MouvementId",
+                table: "EcritureComptables",
+                column: "MouvementId",
+                principalTable: "Mouvement",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EcritureComptables_Mouvement_MouvementId",
+                table: "EcritureComptables");
+
+            migrationBuilder.DropIndex(
+                name: "IX_EcritureComptables_MouvementId",
+                table: "EcritureComptables");
+
+            migrationBuilder.DropColumn(
+                name: "MouvementId",
+                table: "EcritureComptables");
+        }
+    }
+}
---
+++ Collectivite/Migrations/20251210154710_initCreate6.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Collectivite.Migrations
+{
+    /// <inheritdoc />
+    public partial class initCreate6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Prefecture",
+                table: "Communes",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Region",
+                table: "Communes",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Prefecture",
+                table: "Communes");
+
+            migrationBuilder.DropColumn(
+                name: "Region",
+                table: "Communes");
+        }
+    }
+}
