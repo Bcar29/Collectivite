f0780ae8fac2ce6a2a8ff4ccbf207c83d2d8557f
Ibrahim-Sempy <ibrahimsempy@gmail.com>
2025-12-18 13:19:38 +0000

    suppression des migrations

--- Collectivite/Migrations/20251218112110_InitialCreate.Designer.cs
+++ Collectivite/Migrations/20251218112110_InitialCreate.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Collectivite.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20251217140023_initCreate")]
-    partial class initCreate
+    [Migration("20251218112110_InitialCreate")]
+    partial class InitialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
